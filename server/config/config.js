9420664deb830dd3a19954d4507354a56ac909f5
Alejandro Paredes <Alejandro Paredes>
2019-06-17 13:04:58 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -10,6 +10,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://axsheru:NvUzEROM3fPcoL1t@cluster0-yhnzx.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
