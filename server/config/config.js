916a12654b0d43719bbd6273c908ffedc014d552
Alejandro Paredes <Alejandro Paredes>
2019-06-17 12:21:29 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,15 @@
+//PUERTO
 process.env.PORT = process.env.PORT || 3000;
+
+//AMBIENTE
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//BASE DE DATOS
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://axsheru:NvUzEROM3fPcoL1t@cluster0-yhnzx.mongodb.net/cafe';
+}
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -9,39 +10,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
-    res.json('Get usuario');
+app.use(require('./routes/usuario'));
+
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
+    if (err) throw err;
+
+    console.log('DB conectada!');
 });
-
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: "Faltan datos."
-        });
-    } else {
-        res.json({
-            body
-        });
-    }
-
-});
-
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('Delete usuario');
-});
-
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando el puerto: ', 3000);
